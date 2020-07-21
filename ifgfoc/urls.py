1923f444192577d85e2d6feb79beff317931805a
Jonathan Thamrun <jonathan.thamrun@gmail.com>
2020-07-21 16:38:43 -0700

    Changes 3

--- ifgfoc/urls.py
+++ ifgfoc/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("about-us", views.aboutUs, name="aboutUs"),
+    path("give", views.give, name="give"),
+    path("care-group", views.careGroup, name="careGroup"),
+    path("stream", views.stream, name="stream")
 ]
--- ifgfoc/views.py
+++ ifgfoc/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "ifgfoc/index.html")
+
+def aboutUs(request):
+    return render(request, "ifgfoc/aboutUs.html")
+
+def give(request):
+    return render(request, "ifgfoc/give.html")
+
+def careGroup(request):
+    return render(request, "ifgfoc/careGroup.html")
+
+def stream(request):
+    return render(request, "ifgfoc/stream.html")
