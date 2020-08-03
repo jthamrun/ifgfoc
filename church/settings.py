961ab5d2500f2f238c9a2285d1dcfeeff714ab14
widodoalfianto <widodoalfianto94@gmail.com>
2020-08-03 15:34:53 -0700

    Update FrontEnd 08/03/2020

--- church/settings.py
+++ church/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = '++3q$h6t)u6^q#p1&m%e(!mg-je9+@=rn8%*cpjzim3onapiw-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+#'ifgfoc.pythonanywhere.com'
 ALLOWED_HOSTS = ['ifgfoc.pythonanywhere.com']
 
 
