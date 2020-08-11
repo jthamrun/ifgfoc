5e56547f922b643dc31262fafb544401899cd1cb
Jonathan Thamrun <jonathan.thamrun@gmail.com>
2020-08-11 15:47:08 -0700

    FrontEnd Update 08/11/2020

--- ifgfoc/static/js/script.js
+++ ifgfoc/static/js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $.get(
         "https://www.googleapis.com/youtube/v3/channels", {
             part: 'contentDetails',
-            id: 'UCnlbFb18WzWfsiYnmH10IWg', 
-            key: 'AIzaSyDON8FOVrai8e6OlrnNKN5-r9_xktaUEI8' },
+            id: 'UCnlbFb18WzWfsiYnmH10IWg',
+            key: 'AIzaSyBxiln-oRK55Qz_Wh0sbAX1xbeH9oUen_k' },
             function(data) {
                 $.each(data.items, function(i, item) {
                     console.log(item);
@@ -17,42 +17,23 @@ $(document).ready(function(){
         $.get(
             "https://www.googleapis.com/youtube/v3/playlistItems", {
                 part: 'snippet',
-                maxResults: 24,
+                maxResults: 2,
                 playlistId: pid,
-                key: 'AIzaSyDON8FOVrai8e6OlrnNKN5-r9_xktaUEI8' },
+                key: 'AIzaSyBxiln-oRK55Qz_Wh0sbAX1xbeH9oUen_k' },
+                
                 function(data) {
-                    var output = "";
-                    var count = 1;
-                    
-                    output.concat("<div class='row'>")
-
+                    var output;
                     $.each(data.items, function(i, item) {
                         console.log(item);
                         vidTitle = item.snippet.title;
-                        thumbnails_pic = item.snippet.thumbnails.high.url;
-                        vidUrl = item.snippet.resourceId.videoId;
-                        var vidtemp = '<div style="margin-bottom: 50px" class="col l4"><a href="https://www.youtube.com/watch?v='+ vidUrl +'"><img src= "https://img.youtube.com/vi/' + vidUrl + '/mqdefault.jpg">'+ vidTitle +'</a></div>';
-                        
-                        
-                        if (count%3 == 0) {
-                            output.concat(vidtemp).concat('</div><div class="row">');
-                        }
-                        else {
-                            output.concat('<div style="margin-bottom: 50px" class="col l4"><a href="https://www.youtube.com/watch?v='+ vidUrl +'"><img src= "https://img.youtube.com/vi/' + vidUrl + '/mqdefault.jpg">'+ vidTitle +'</a></div>');
-                        }
+                        vidThumbnails = item.snippet.thumbnails.high.url;
+                        vidURL = item.snippet.resourceId.videoId;
 
-                        if(count==24) {
-                            output = output.substr(output.length - 17);
-                        }
-                        
-                        count += 1;
+                        output = '<div class="row"><div class="col l6 s8 push-s2 center-align"><a href="https://www.youtube.com/watch?v=' + vidURL + '"><img src="' + vidThumbnails + '" width=450vw height=400vh></a></div></div>';
 
-                        // output = '<div class="col l4"><a href="https://www.youtube.com/watch?v='+ vidUrl +'"><img src= "' + thumbnails_pic + '" width="300px" height="300px"></a></div>';
-
-                        
                         $('#results').append(output);
-                  })
-              }
+                    })
+                }
         );
     }
 });
