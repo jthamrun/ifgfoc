8464119c68fd501420b380f7331a4ec4092bb591
widodoalfianto <widodoalfianto94@gmail.com>
2020-08-15 18:13:42 -0700

    Revamp + Consolidate CSS

--- ifgfoc/urls.py
+++ ifgfoc/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about-us", views.aboutUs, name="aboutUs"),
     path("give", views.give, name="give"),
-    path("care-group", views.careGroup, name="careGroup"),
+    path("care-group", views.caregroup, name="care-group"),
     path("stream", views.stream, name="stream")
 ]
